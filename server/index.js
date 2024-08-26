489915bb73d4538bed1caedbe20828a20e0c075f
Khwaja Shaikh <109279460+Shkkhwaja@users.noreply.github.com>
2024-08-26 18:46:30 +0530

    deploy

--- server/index.js
+++ server/index.js
@@ -2,19 +2,18 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import userData from "./model/user.model.js";
-import dotenv from "dotenv"
+import 'dotenv/config'
 
 const app = express();
-app.use(dotenv())
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect("mongodb://127.0.0.1:27017/employee");
+mongoose.connect(`${process.env.MONGO_DB}`);
 
 app.post("/login", (req, res) => {
     const { user, pass } = req.body;
     userData.findOne({ user: user }).then((users) => {
-      if (usersz) {
+      if (users) {
         if (users.pass === pass) {
           res.json("Success");
         } else {
